322ade0b50bc29eaa9bf0565dd3896aafdbdd43f
Arpit Suthar <nikhilsuthar@gmail.com>
2018-01-11 10:46:42 +0530

    Corrected Spring backend

--- src/main/java/in/arpit/spring/angularboot/AngularBootApplication.java
+++ src/main/java/in/arpit/spring/angularboot/AngularBootApplication.java
@@ -3,47 +3,43 @@ package in.arpit.spring.angularboot;
 import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
+@EnableResourceServer
 public class AngularBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(AngularBootApplication.class, args);
     }
 
-
-    @Configuration
-    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-
-            http.antMatcher("/**").authorizeRequests()
-                    .antMatchers("/", "/*.css", "/*.js").permitAll()
-                    .anyRequest().authenticated();
-        }
+    @Bean
+    protected ResourceServerConfigurerAdapter resourceServerConfigurerAdapter() {
+        return new ResourceServerConfigurerAdapter() {
+            @Override
+            public void configure(HttpSecurity http) throws Exception {
+                http.authorizeRequests()
+                        .antMatchers("/", "/*.css", "/*.js").permitAll()
+                        .anyRequest().authenticated();
+            }
+        };
     }
 
     @Bean
     public EmbeddedServletContainerCustomizer cookieProcessorCustomizer() {
         return container -> {
             if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                LegacyCookieProcessor legacyCookieProcessor = new LegacyCookieProcessor();
+                legacyCookieProcessor.setAllowHttpSepsInV0(true);
                 ((TomcatEmbeddedServletContainerFactory) container)
                         .addContextCustomizers((TomcatContextCustomizer)
-                                context -> context.setCookieProcessor(new LegacyCookieProcessor()));
+                                context -> context.setCookieProcessor(legacyCookieProcessor));
             }
         };
     }
--- src/main/java/in/arpit/spring/angularboot/controller/HomeController.java
+++ src/main/java/in/arpit/spring/angularboot/controller/HomeController.java
@@ -3,9 +3,14 @@ package in.arpit.spring.angularboot.controller;
 import com.google.common.collect.Lists;
 import in.arpit.spring.angularboot.entity.Note;
 import in.arpit.spring.angularboot.repository.NotesRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 
 import java.security.Principal;
 import java.util.List;
@@ -13,12 +18,22 @@ import java.util.List;
 @RestController
 public class HomeController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    OAuth2ClientContext clientContext;
+
     @Autowired
     private NotesRepository repository;
 
     @GetMapping("/note")
+    @PreAuthorize("hasAuthority('Everyone')")
     public List<Note> home(Principal principal){
-        List<Note> notes = repository.findAllByUser(principal.getName());
+        LOGGER.info(principal.getName());
+        LOGGER.info(((OAuth2Authentication) principal).getUserAuthentication().getAuthorities().toString());
+        LOGGER.info(String.valueOf(((OAuth2Authentication) principal).isAuthenticated()));
+        LOGGER.info(String.valueOf(clientContext.getAccessToken().isExpired()));
+        List<Note> notes = repository.findAll();
         if (notes.isEmpty()) {
             return Lists.newArrayList();
         } else {
